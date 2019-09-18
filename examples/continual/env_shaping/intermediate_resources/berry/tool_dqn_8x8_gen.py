2cd97b73be7853d065cdc553c4c4ff9f17fd77b0
Suvansh Sanjeev <suvansh@berkeley.edu>
2019-09-18 12:58:53 -0700

    adding a validation stats script

--- examples/continual/env_shaping/intermediate_resources/berry/tool_dqn_8x8_gen.py
+++ examples/continual/env_shaping/intermediate_resources/berry/tool_dqn_8x8_gen.py
@@ -26,7 +26,7 @@ from rlkit.samplers.data_collector import MdpPathCollector
 from rlkit.torch.torch_rl_algorithm import TorchBatchRLAlgorithm, TorchLifetimeRLAlgorithm
 
 # from variants.dqn.dqn_medium_mlp_task_partial_variant import variant as algo_variant, gen_network
-from variants.dqn_lifetime.dqn_medium8_mlp_task_partial_variant import variant as algo_variant, gen_network
+from variants.dqn_lifetime.dqn_medium8_mlp_task_partial_variant import variant as algo_variant, gen_network_num_obj as gen_network
 
 
 def schedule(t):
@@ -178,7 +178,7 @@ if __name__ == "__main__":
             min_num_steps_before_training=200,
             max_path_length=math.inf,
             batch_size=256,
-            validation_envs_pkl=join(get_repo_dir(), 'examples/continual/env_shaping/intermediate_resources/berry/validation_envs/dynamic_static_validation_envs_8x8_2019_09_09_13_43_00.pkl'),
+            validation_envs_pkl=join(get_repo_dir(), 'examples/continual/env_shaping/intermediate_resources/berry/validation_envs/dynamic_static_validation_envs_8x8_2019_09_18_04_41_47.pkl'),
             validation_rollout_length=300
         ),
         trainer_kwargs=dict(
@@ -197,6 +197,12 @@ if __name__ == "__main__":
             input_size=200,
             output_size=32,
             hidden_sizes=[64, 64]
+        ),
+        num_obj_network_kwargs=dict(
+            # num_objs: 8
+            input_size=8,
+            output_size=8,
+            hidden_sizes=[8]
         )
     )
     algo_search_space = copy.deepcopy(algo_variant)
@@ -225,5 +231,5 @@ if __name__ == "__main__":
                     region='us-west-2',
                     num_exps_per_instance=3,
                     snapshot_mode='gap',
-                    snapshot_gap=25
+                    snapshot_gap=10
                 )
